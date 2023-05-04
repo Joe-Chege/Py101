9e0e3c8c7cd48265671483e4a90fb127f8143c85
Joe-Chege <njorogejosephchege@gmail.com>
2023-05-04 20:57:11 +0300

    types of strings

---
+++ Data-in-python/strings.py
@@ -0,0 +1,17 @@
+# Strings >> A collection of characters is termed as a string
+#It is a collection of characters closed within single, double or triple quotation marks.
+
+# Here is a good example of stings 
+
+print('Am loving every bit coding in python') # single quatation
+
+doubles = "In double quotes there is still sn output..." # double quotation
+print(doubles)
+
+print("$") # single character
+
+nill = ""  # prints an empty space hence note that python is sentitive with even space 
+print(nill) # prints an empty line
+
+Triple = """ this is tiple quatation string"""
+print(Triple)
