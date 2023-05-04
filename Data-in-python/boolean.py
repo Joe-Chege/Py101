e2bc6623dfc85e3d64fc3cd4147147fc9a84ed29
Joe-Chege <njorogejosephchege@gmail.com>
2023-05-04 08:38:23 +0300

    Boolean

---
+++ Data-in-python/boolean.py
@@ -0,0 +1,14 @@
+# Booleans
+# This data type allows us to choose between two values: true and false.
+""" A Boolean is used to determine whether the logic of an expression or a comparison is correct.
+ By showing whether is true or false"""
+
+ ## Here is an example of Boolean code 
+
+print(True)
+
+f_bool = False
+print(f_bool)
+
+
+# Note that true and false first letters are in capital
