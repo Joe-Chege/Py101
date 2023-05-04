251be8164d2d477fe22337de048ec254b0bc6c97
Joe-Chege <njorogejosephchege@gmail.com>
2023-05-04 07:55:10 +0300

    integers and floating point numbers

---
+++ digits.py
@@ -0,0 +1,30 @@
+""" Numbers 
+Python is one of the most powerful languages when it comes to manipulating numerical data.
+The numericals imclude integers , floating points and complex numbers  """
+# integers >> The integer data type is comprised of all the positive and negative whole numbers.
+#The amount of memory an integer occupies depends on its value. 
+#For example, 0 will take up 24 bytes whereas 1 would occupy 28 bytes
+
+#Here is a good examples when it comes to integers
+
+print(10)  # A positive integer
+print(-3000)  # A negative integer
+
+num = 123456789  # Assigning an integer to a variable
+print(num)
+num = -16000  # Assigning a new integer
+print(num)
+
+""" Floating Point Numbers
+Floating-point numbers, or floats, refer to positive and negative decimal numbers.
+A float occupies 24 bytes of memory. """
+
+# Here is an example of floating point numbers
+
+print(1.00000000005)  # A positive float
+print(-85.6701)  # A negative float
+
+flt_pt = 1.23456789
+print(flt_pt)
+
+
