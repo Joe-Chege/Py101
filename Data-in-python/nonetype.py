c6974b6480930fff435c7f6c68be95e00d3ecdf6
Joe-Chege <njorogejosephchege@gmail.com>
2023-05-04 21:50:36 +0300

    nonetype

---
+++ Data-in-python/nonetype.py
@@ -0,0 +1,15 @@
+## Nonetype >> It only has a single value, None
+"""
+Important Points
+None is not a default value for the variable that has not yet been assigned a value.
+None is not the same as False.
+None is not an empty string.
+None is not 0.
+
+"""
+# Here is an example of nonetype
+
+val = None
+print(val) # prints "None" and returns None
+print (type(val))
+
