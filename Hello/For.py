0b919cb6c553725bdc699b63a4f9f889a328b41b
Joe-Chege <njorogejosephchege@gmail.com>
2023-06-19 11:05:03 +0300

    Using range fuction in for loop

---
+++ Hello/For.py
@@ -0,0 +1,11 @@
+"""Loops come in handy Especially to avoid repetitions"""
+# For loops works well with other fuction like range
+
+# Here is an example using range
+
+for i in range(1, 11):
+    print(i) #One has to call i for this code basically a fuction
+
+# The code prints from 1 to 10. Something worth noting is that indexing comes in handy
+
+
