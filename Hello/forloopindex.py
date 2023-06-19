656f0d9c422c5683926cc01c38cfdb6cc948229f
Joe-Chege <njorogejosephchege@gmail.com>
2023-06-19 11:11:17 +0300

    printing odd numbers observing the indexing option

---
+++ Hello/forloopindex.py
@@ -0,0 +1,9 @@
+# Through indexing one can skip the range of inters being printed
+
+#Here is an example
+
+for i in range(1,20,2):
+    print(i)
+
+# When one prints this code it will print the sequence skiping 2,4,6... it will print the odd numbers
+
