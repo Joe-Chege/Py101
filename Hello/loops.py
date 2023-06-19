99eeade93563d34fdc145e34ae13f26d73c6a236
Joe-Chege <njorogejosephchege@gmail.com>
2023-06-19 10:38:25 +0300

    Python A to Z

---
+++ Hello/loops.py
@@ -0,0 +1,17 @@
+# In understanding loops how to use elif, if and else statement  
+
+a = int(input("Enter the value of a "))
+b = int(input("Enter the value of b "))
+
+if (a>b): 
+    print("A is greater")
+elif (b>a):
+    print("B is greater")
+
+else:
+    print("Both are equal")
+
+"""The code has used the input fuction and int showing it expects an integer 
+Conditional operators are evident in the if, else and elif statement """
+
+
