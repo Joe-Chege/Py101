8c04e6d4b177f64319a9394f05e203ce86b1b520
Joe-Chege <njorogejosephchege@gmail.com>
2023-06-20 17:16:28 +0300

    tables with while loop

---
+++ Hello/whileloop.py
@@ -0,0 +1,8 @@
+# while tend to use <= (less than or equal to operations to meet its requirment)
+
+a = int(input("enter a val "))
+i = 1
+while (i <= 10):
+    print(a, "x",i,"=" ,i*a)
+
+    i = i + 1
