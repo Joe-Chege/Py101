ca6887517e376190da9b5000acfd65013376e55f
Joe-Chege <njorogejosephchege@gmail.com>
2023-06-23 11:30:12 +0300

    Floats

--- Hello/letsadd.py
+++ Hello/letsadd.py
@@ -2,8 +2,10 @@
 sum = 0
 i = 1
 
-while i<=300:
+while i<=25:
     sum = sum + i
     i = i + 1
     print (sum)
+
+
 
---
+++ Hello/miles.py
@@ -0,0 +1,10 @@
+mile = int(input("Enter the value in miles: "))
+
+# convering miles into km
+# km_mile=miles x 1.609344
+mile_float = float(mile)
+
+km_mile = (mile_float * 1.609344)
+
+print("The value of miles in km " )
+print(km_mile)
